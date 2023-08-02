efb528abcc1608f236f31fe773c86f6365873064
yerti <yerti0818@gmail.com>
2023-08-01 17:14:16 -0700

    prueba

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,9 @@ class BaseModel:
     """The BaseModel class of future classes"""
     def __init__(self):
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -20,12 +20,12 @@ class BaseModel:
 
     def save(self):
         """Updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """Updates the attribute 'updated_at' with the current datetime"""
+        new_dict["__class__"] = self.__class__.__name__
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at.strtime("%Y-%m-%dT%H:%M:%S.%f")
         new_dict["updated_at"] = self.updated_at.strtime("%Y-%m-%dT%H:%M:%S.%f")
-        new_dict["__class__"] = self.__class__.__name__
         return new_dict
