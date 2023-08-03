3a598c48bef8ddf57bd936ab783ad53b0f521c67
Denzel18rb <74129875rb@gmail.com>
2023-08-03 15:38:29 -0700

    azuuuuuuucar

--- console.py
+++ console.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """text"""
     def emptyline(self):
         """text"""
         pass
 
-
     completekey = None
-    
     prompt = '(hbnb) '
 
     def __init__(self):
@@ -33,6 +32,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
