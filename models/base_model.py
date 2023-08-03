04165eb4f1218f1c9e0b66d5cb4c9183b9c4b578
Denzel18rb <74129875rb@gmail.com>
2023-08-03 10:26:54 -0700

    papa rellena

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 """
-Contains class BaseModel >:v
-"""
-import uuid
-import datetime
+gaaaaa >:v
 
+"""
+from datetime import datetime
+from uuid import uuid4
 
 class BaseModel:
-    """The BaseModel class of future classes"""
-    def __init__(self):
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """constructor"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -20,12 +28,12 @@ class BaseModel:
 
     def save(self):
         """Updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Updates the attribute 'updated_at' with the current datetime"""
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict["__class__"] = self.__class__.__name__
+        new_dict.pop("__class__", None)
         return new_dict
