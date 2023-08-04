91f49f9311b935e5ee14b5d55839c6bd808edae8
yerti <yerti0818@gmail.com>
2023-08-04 11:28:43 -0700

    resolver el error

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """
-gaaaaa >:v
-
+create new class
 """
+
+import models
 from datetime import datetime
 from uuid import uuid4
+
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """constructor"""
         if kwargs:
             for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
                 if key == "updated_at":
                     self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == "created_at":
                     self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                else:
-                    setattr(self, key, value)
+            
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -29,11 +33,12 @@ class BaseModel:
     def save(self):
         """Updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Updates the attribute 'updated_at' with the current datetime"""
         new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict.pop("__class__", None)
         return new_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+
+    
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__,obj.id)
+        FileStorage.__objects[key] = obj.to_dict()
+
+    def save(self):
+        objts_dict = {}
+
+        for key, obj in FileStorage.__objects.items():
+            objts_dict[key] = obj
+
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(objts_dict, file)
+
+    def reload(self):
+        if os.path.exists(FileStorage.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                alm = f.read()
+                dictr = json.loads(alm)
+                for key, value in dictr.items():
+                    value = dictr[key]
+                    obj = eval(value['__class__'])(**value)
+                    FileStorage.__objects[key] = obj
+        else:
+            pass
